3bbbd74c2da3ff7a110d997b31e331cfe9059110
Will <nguyenthaiqui233@gmail.com>
2020-12-16 11:16:34 +0700

    Add cloudinary

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -1,16 +1,37 @@
 const path = require('path')
 const _ = require('lodash')
-const Resize = require('./resize')
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+// const Resize = require('./resize')
+
+cloudinary.config({
+  cloud_name: 'willnguyen',
+  api_key: '245577489324558',
+  api_secret: 'mnFdyV_JAoY8b-xFmjuVcHz2NWw'
+})
+
+const streamUpload = (req) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      (error, result) => {
+        if (result)
+          resolve(result)
+        else
+          reject(error)
+      }
+    )
+
+    streamifier.createReadStream(req.file.buffer).pipe(stream)
+  })
+}
 
 exports.upload = async (req, res) => {
-  // folder upload
-  let imagePath = path.join(__dirname, '../public/images')
-  // call class Resize
-  const fileUpload = new Resize(imagePath);
-  if (!req.file) {
-    res.status(401).json({ error: 'Please provide an image' });
-  }
-  const filename = await fileUpload.save(req.file.buffer);
-
-  return res.status(200).json({ name: filename });
+  const result = await streamUpload(req)
+  return res.status(200).json({
+    originalSize: {
+      width: result.width,
+      height: result.height,
+    },
+    url: result.url,
+  });
 }
